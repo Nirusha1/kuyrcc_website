fad95b7f4b953c53696965654c7db3b834a3a870
Nirusha1 <32360818+Nirusha1@users.noreply.github.com>
2019-06-10 11:51:16 +0545

    Update volunteers.js

--- routes/volunteers.js
+++ routes/volunteers.js
@@ -8,25 +8,25 @@ let contactVariable = require('../models/contacts');
 let volunteerVariable=require('../models/volunteers');
 
 //add routes for creating events
-router.get('/CreateVolunteers', function(req, res){
-	res.render('volunteers',{
-		title:'volunteers'
+router.get('/CreateVolunteers/:eventid', function(req, res){
+	eventVariable.findById(req.params.eventid, function(err, events){
+		res.render('volunteers',{
+			events: events
+		});
 	});
 });
 
-router.post('/CreateVolunteers', function(req, res){
+router.post('/CreateVolunteers/:eventid', function(req, res){
 	console.log('submitted Volunteer form');
 	let x= new volunteerVariable();
-	x.volunteer_eventname=req.Events.event_name;
-	x.volunteer_eventHolder = req.body.volunteer_name;	//to show who has posted the event
-	x.volunteer_eventid = req.body.volunteer_name;
+	x.volunteer_eventid=req.params.eventid;
 	x.volunteer_name=req.body.volunteer_name;
 	x.volunteer_address=req.body.volunteer_address;
 	x.volunteer_phone=req.body.volunteer_phone;
 	x.volunteer_fb=req.body.volunteer_fb;
 	x.volunteer_email=req.body.volunteer_email;
 
-	
+	let query = {_id:req.params.id}
 
 	x.save(function(err){
 		if(err){
@@ -42,16 +42,19 @@ router.post('/CreateVolunteers', function(req, res){
 });
 
 
-router.get('/ViewVolunteers', function(req, res){
+router.get('/ViewVolunteers/:eventid', function(req, res){
 	volunteerVariable.find({}, function(err, volunteers){
 		if(err){
 			console.log(err);
 		}
 		else{
-			res.render('view_volunteers',{
-				title:'All Events',
-				volunteers : volunteers
+			eventVariable.findById(req.params.eventid, function(err, events){
+				res.render('view_volunteers',{
+					title:'All Events',
+					volunteers : volunteers,
+					events:events
 
+				});
 			});
 		}
 	});
